e936a477922c135a46caeeb6473b55f10de724a9
Aldosebas28 <a20110199@ceti.mx>
2022-09-05 11:47:47 -0500

    Practica 18
    Diferencias entre TUPLAS y LISTAS

---
+++ 18_TUPLAS/18_TUPLAS/_18_TUPLAS.py
@@ -0,0 +1,9 @@
+lista = ['Rojo','Azul','Verde','Amarillo']
+
+tupla = (10,15,20,"El resultado de la operacion es: ")#Las tuplas no pueden cambiar y es necesario de una coma
+
+print(tupla[3],tupla[0]+tupla[2])
+
+lista.append(50)
+
+print(lista)
