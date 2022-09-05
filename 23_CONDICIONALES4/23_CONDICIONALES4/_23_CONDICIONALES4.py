18daced9e29180f2fab0e36d32f52dc46208381d
Aldosebas28 <a20110199@ceti.mx>
2022-09-05 13:22:02 -0500

    Practica 23
    Comprobar datos en listas y tuplas

---
+++ 23_CONDICIONALES4/23_CONDICIONALES4/_23_CONDICIONALES4.py
@@ -0,0 +1,6 @@
+entrada = input('Introduce el nombre de un navegador:\n')
+navegadores = ['CHROME', 'FIREFOX', 'EDGE', 'SAFARI']
+if entrada in navegadores:
+    print('El navegador que buscas está en la lista.')
+else:
+    print('El navegador que buscas no está en la lista.')
