4ad6f302d5a97986b9042ec1443cda6d10140e92
Aldosebas28 <a20110199@ceti.mx>
2022-09-05 11:48:23 -0500

    Practica 19
    Convertir tuplas a listas y viceversa

---
+++ 19_TUPLAS_A_LISTAS/19_TUPLAS_A_LISTAS/_19_TUPLAS_A_LISTAS.py
@@ -0,0 +1,11 @@
+lista = ["Rojo","Azul","Verde","Amarillo"]#En corchetes es una lista 
+
+tupla = tuple(lista)#En parentesis es una tupla
+
+print(tupla)
+
+tupla = ("Rojo","Azul","Verde","Amarillo") 
+
+lista = list(tupla)
+
+print(lista)
