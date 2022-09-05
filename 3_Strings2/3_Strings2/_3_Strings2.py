a25f3a0eb092d813136ab1c9ae166689ca943260
Aldosebas28 <a20110199@ceti.mx>
2022-09-05 09:32:35 -0500

    Practica 3
    Concatenar Strings

---
+++ 3_Strings2/3_Strings2/_3_Strings2.py
@@ -0,0 +1,10 @@
+primeraPalabra="Programación"
+
+segundaPalabra= "Fácil"
+
+nombreCanal = primeraPalabra +" "+ segundaPalabra
+
+print(nombreCanal)
+
+print("Este es el canal de "+"Programación Fácil")
+
