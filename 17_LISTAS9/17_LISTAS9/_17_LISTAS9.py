69e782239c8088b1d2636580dde11f3c5c1dbc0b
Aldosebas28 <a20110199@ceti.mx>
2022-09-05 11:21:48 -0500

    Practica 18
    Contar elementos de una lista con LEN()

---
+++ 17_LISTAS9/17_LISTAS9/_17_LISTAS9.py
@@ -0,0 +1,6 @@
+paises = ['Mexico','USA','Afganistan','Alemania','Angola','India','Argetina','Chile','Colombia','Marruecos',
+         'Nicaragua','PR','Moscow','Belgica','Brasil','Finlandia','Francia','Antartica',"Hong Kong","Tokio"]
+
+print(len(paises))
+
+print(paises[19])
